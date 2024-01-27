fe9db6f6dfc877a632d53dd90d9a38a1fb5e8c17
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2024-01-27 05:35:59 +0000

    chore: mvn boilerplate

---
+++ src/test/java/cs3173/construction/compiler/AppTest.java
@@ -0,0 +1,20 @@
+package cs3173.construction.compiler;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
